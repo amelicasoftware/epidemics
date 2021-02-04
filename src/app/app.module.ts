958750f37120f47761e79854fa344ab14afff438
Mario Flores <floresmario96@hotmail.com>
2021-02-04 15:16:09 -0600

    Archivo de rutas agregado a los imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BusquedaGeneralComponent } from './pages/busqueda-general/busqueda-general.component';
+import { ROUTES } from './routes/app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusquedaGeneralComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
